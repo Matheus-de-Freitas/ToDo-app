f80903ba3c475af555d6b055b39795f029b6a7c9
Matheus de Freitas <matheusfreitas.a09@gmail.com>
2023-11-22 16:31:19 -0300

    criação - primeira view do projeto

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const exphbs = require("express-handlebars")
 
 const app = express()
 
+app.engine('handlebars' , exphbs.engine())
+app.set('view engine' , 'handlebars')
+
 app.get('/' , (requisicao, resposta) => {
-    resposta.send("Olá mundo!")
+    resposta.render('home')
 })
 
 app.listen(3000, () => {
