44288bfeb37ddf4ebda282d89c7e98df8b54c862
Matheus de Freitas <matheusfreitas.a09@gmail.com>
2023-11-28 16:58:29 -0300

    Filtrando as tarefas ativas

--- index.js
+++ index.js
@@ -34,6 +34,24 @@ app.post('/completar' , (requisicao, resposta) => {
     })
 })
 
+app.post("/descompletar" , (requisicao, resposta) => {
+    const id = requisicao.body.id 
+
+    const sql = `
+        UPDATE tarefas
+        SET completa = '0'
+        WHERE id = ${id}
+    `
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
 app.post('/criar' , (requisicao, resposta) => {
     const descricao = requisicao.body.descricao
     const completa = 0
@@ -71,8 +89,14 @@ app.get('/' , (requisicao, resposta) => {
             }
         })
 
+        const tarefasAtivas = tarefas.filter((tarefa) => {
+            return tarefa.completa === false && tarefa
+        })
 
-        resposta.render('home' , {tarefas})
+        const quantidadeTarefasAtivas = tarefasAtivas.length
+
+
+        resposta.render('home' , { tarefas, quantidadeTarefasAtivas })
     })
 
 })
